91c46de6cba8aad5d22a8f23f699541976dd4e67
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-16 16:30:41 -0300

    requisitos 1,2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,19 @@
 export const fetchProduct = () => {
   // seu código aqui
+
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = async (param) => {
   // seu código aqui
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (!fetchProduct) {
+      throw new Error('Termo de busca não informado');
+    }
+    return data.results;
+  } catch (erro) {
+    Error('Termo de busca não informado');
+  }
 };
