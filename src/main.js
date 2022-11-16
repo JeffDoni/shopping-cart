59de22654ac98e7fd6ef2e4fa3f8dfc726800ce9
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-16 19:44:18 -0300

    Exercico 3,4

--- src/main.js
+++ src/main.js
@@ -4,15 +4,28 @@ import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-// const products = document.querySelector('.products');
+const products = document.querySelector('.products');
+const p = document.createElement('p');
 
-// const imprime = async () => {
-//   const dados = await fetchProductsList('computador');
-//   const { results } = dados;
-//   results.forEach(({ id, title, thumbnail, price }) => {
-//     const obj = { id, title, thumbnail, price };
-//     products.appendChild(createProductElement(obj));
-//   });
-// };
+const loading = () => {
+  p.className = 'loading';
+  p.textContent = 'carregando...';
+  products.appendChild(p);
+};
 
-// imprime();
+const deletP = () => p.remove();
+
+const imprime = async () => {
+  loading();
+  const dados = await fetchProductsList('computador');
+  deletP();
+  return dados.forEach((element) => {
+    const result = createProductElement(element);
+    console.log(result);
+    products.appendChild(result);
+  });
+};
+
+window.onload = () => {
+  imprime();
+};
