0ab89ca2cee6442697b0d33db0e9e6964fe67446
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-16 19:08:01 -0300

    requisitos

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-const products = document.querySelector('.products');
+// const products = document.querySelector('.products');
 
-const imprime = async () => {
-  const dados = await fetchProductsList('computador');
-  const { results } = dados;
-  return results.forEach(({ id, title, thumbnail, price }) => {
-    const obj = { id, title, thumbnail, price };
-    products.appendChild(createProductElement(obj));
-  });
-};
+// const imprime = async () => {
+//   const dados = await fetchProductsList('computador');
+//   const { results } = dados;
+//   results.forEach(({ id, title, thumbnail, price }) => {
+//     const obj = { id, title, thumbnail, price };
+//     products.appendChild(createProductElement(obj));
+//   });
+// };
 
-imprime();
+// imprime();
