4e018f8168c07d879d01859122e4d46d0d437262
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2022-11-16 21:18:29 -0300

    Requisito5

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const products = document.querySelector('.products');
 const p = document.createElement('p');
+const h1 = document.createElement('h1');
+
+const createdError = () => {
+  h1.className = 'error';
+  h1.textContent = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  products.appendChild(h1);
+};
 
 const loading = () => {
   p.className = 'loading';
@@ -17,14 +24,31 @@ const deletP = () => p.remove();
 
 const imprime = async () => {
   loading();
-  const dados = await fetchProductsList('computador');
-  deletP();
-  return dados.forEach((element) => {
-    const result = createProductElement(element);
-    console.log(result);
-    products.appendChild(result);
-  });
+  try {
+    const dados = await fetchProductsList('computador');
+    deletP();
+    dados.forEach((element) => {
+      const result = createProductElement(element);
+      console.log(result);
+      products.appendChild(result);
+    });
+  } catch (error) {
+    console.log('erro');
+    deletP();
+    createdError();
+  }
 };
+
+// const imprime = async () => {
+//   loading();
+//   const dados = await fetchProductsList('computador');
+//   deletP();
+//   return dados.forEach((element) => {
+//     const result = createProductElement(element);
+//     console.log(result);
+//     products.appendChild(result);
+//   })
+// };
 
 window.onload = () => {
   imprime();
